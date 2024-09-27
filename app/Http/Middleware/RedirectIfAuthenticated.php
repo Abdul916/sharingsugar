f2bf3afb6b95734440fe14cb571b8f6d07a96238
abdul <abdulwaheedsharif@gmail.com>
2024-09-27 17:11:45 +0500

    //

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -17,32 +17,32 @@ class RedirectIfAuthenticated
      * @param  string|null  ...$guards
      * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
      */
+
+    // public function handle(Request $request, Closure $next, ...$guards)
+    // {
+    //     $guards = empty($guards) ? [null] : $guards;
+    //     foreach ($guards as $guard) {
+    //         if (Auth::guard($guard)->check()) {
+    //             return redirect(RouteServiceProvider::HOME);
+    //         }
+    //     }
+    //     return $next($request);
+    // }
+
     public function handle(Request $request, Closure $next, ...$guards)
     {
         $guards = empty($guards) ? [null] : $guards;
-
         foreach ($guards as $guard) {
-            if (Auth::guard($guard)->check()) {
-                return redirect(RouteServiceProvider::HOME);
+            if($guard == 'admin'){
+                if (Auth::guard($guard)->check()) {
+                    return redirect('admin/dashboard');
+                }
+            }else{
+                if (Auth::guard($guard)->check()) {
+                    return redirect(RouteServiceProvider::HOME);
+                }
             }
         }
         return $next($request);
     }
-
-    // public function handle($request, Closure $next, $guard = null)
-    // {
-    //     switch($guard){
-    //         case 'admin':
-    //         if (Auth::guard($guard)->check()) {
-    //             return redirect('/admin');
-    //         }
-    //         break;
-    //         default:
-    //         if (Auth::guard($guard)->check()) {
-    //             return redirect('/');
-    //         }
-    //         break;
-    //     }
-    //     return $next($request);
-    // }
 }
